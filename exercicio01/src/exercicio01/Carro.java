029b3d548ce1771e4b9721daf6d4dd6f2c62463e
Camille <ferreiracamille819@gmail.com>
2025-03-05 17:20:05 -0300

    Add files via upload

---
+++ exercicio01/src/exercicio01/Carro.java
@@ -0,0 +1,35 @@
+package exercicio01;
+
+public class Carro {
+
+    private final String marca;
+    private final String modelo;
+    private final int ano;
+    private final double preco;
+
+    public Carro(String marca, String modelo, int ano, double preco) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.preco = preco;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void exibirInformacoes() {
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Ano: " + ano);
+        System.out.println("Preço: R$ " + preco);
+    }
+}
---
+++ exercicio01/src/exercicio01/Exercicio01.java
@@ -0,0 +1,10 @@
+package exercicio01;
+
+public class Exercicio01 {
+
+    public static void main(String[] args) {
+      
+        Carro meuCarro = new Carro("Nissan", "Kicks", 2020, 105000.00);
+        meuCarro.exibirInformacoes();
+    }
+}
