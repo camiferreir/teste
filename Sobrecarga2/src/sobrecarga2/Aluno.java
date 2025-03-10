070b073f5009551f0a20bd4e5d1c1acf36da88d1
Camille <ferreiracamille819@gmail.com>
2025-03-10 16:15:26 -0300

    Add files via upload

---
+++ Sobrecarga2/src/sobrecarga2/Aluno.java
@@ -0,0 +1,14 @@
+
+package sobrecarga2;
+
+
+public class Aluno {
+
+    public Aluno(String joão, int i, String a12345) {
+    }
+
+    void exibirInformacoes() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
